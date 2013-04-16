4f47c4c90a592315a9609ecaa5ef7f082dfd1423
Jack Rasmussen <jackbrasmussen@gmail.com>
2013-04-16 09:09:00 +0200

    First classes with variables

---
+++ Bejerholm/src/Model/Ordre.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jack
+ */
+public class Ordre {
+    private int status;
+    private Date bestillingsDato;
+    private Date leveringsDato;
+    private String skrifttype;
+    private int skriftstørrelse;
+    private String bemærkninger;
+    private Double totalPris;
+    private Double MILJØ_AFGIFT;
+    private Double moms;
+    
+}
