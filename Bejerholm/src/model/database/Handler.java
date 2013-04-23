e531b0c3de90b3d7ab6e89a1bc21ca89ab7316c1
Dan-Philip Christensen <anubiz03@hotmail.com>
2013-04-23 12:10:15 +0200

    Ny handler

--- Bejerholm/src/model/database/Handler.java
+++ Bejerholm/src/model/database/Handler.java
@@ -1,17 +1,29 @@
 package model.database;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.sql.Statement;
 
 public class Handler {
     
-    DBConnection conn;
+    Connection conn;
 
-    public Handler(DBConnection conn) {
+    public Handler(Connection conn) {
         this.conn = conn;
     }  
+    
+    public ResultSet soegKunder(int tlfNr) throws SQLException{
+        Statement stmt = conn.createStatement();
+        
+        String command = ("select * from Kunde where tlfNr = " + tlfNr + ";");
+        stmt.execute(command);
+        
+        ResultSet rs = stmt.getResultSet();
+        
+        stmt.close();
+        return rs;
+    }
 }
 
 
