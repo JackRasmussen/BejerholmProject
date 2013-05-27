47e4febcfccf86e1b806471543ba5b469d1cb76d
Nicolai <nicolai@nicolai-laptop.(none)>
2013-05-27 13:27:03 +0200

    Ændret design på BestillingsOrdreGUI

--- Bejerholm/src/view/ordre/BestillingsOrdreGUI.java
+++ Bejerholm/src/view/ordre/BestillingsOrdreGUI.java
@@ -4,17 +4,22 @@
  */
 package view.ordre;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  *
  * @author TheToiletpaper
  */
 public class BestillingsOrdreGUI extends javax.swing.JPanel {
-
-    /**
-     * Creates new form BestillingsOrdreGUI
-     */
+    private Date dateAndTime;
+    
+    
     public BestillingsOrdreGUI() {
         initComponents();
+        
+        dateAndTime = Calendar.getInstance().getTime();
+        jLabel9.setText(dateAndTime.toString());
     }
 
     /**
@@ -32,7 +37,7 @@ public class BestillingsOrdreGUI extends javax.swing.JPanel {
         jComboBox2 = new javax.swing.JComboBox();
         jTextField2 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
+        jTextField1 = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
@@ -46,48 +51,57 @@ public class BestillingsOrdreGUI extends javax.swing.JPanel {
         jTextArea3 = new javax.swing.JTextArea();
         jLabel7 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jCheckBox4 = new javax.swing.JCheckBox();
+        jTextField3 = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
+        jComboBox4 = new javax.swing.JComboBox();
+        jLabel12 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(100, 100, 100));
         setLayout(null);
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         add(jComboBox1);
-        jComboBox1.setBounds(150, 240, 140, 20);
+        jComboBox1.setBounds(450, 60, 140, 27);
 
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Inskription:");
         add(jLabel1);
-        jLabel1.setBounds(90, 60, 120, 14);
+        jLabel1.setBounds(90, 130, 120, 17);
 
         jCheckBox3.setBackground(new java.awt.Color(100, 100, 100));
         jCheckBox3.setForeground(new java.awt.Color(255, 255, 255));
         jCheckBox3.setText("Plads til ekstra navn");
         add(jCheckBox3);
-        jCheckBox3.setBounds(90, 280, 210, 23);
+        jCheckBox3.setBounds(40, 100, 210, 21);
 
         jComboBox2.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         add(jComboBox2);
-        jComboBox2.setBounds(150, 320, 140, 20);
+        jComboBox2.setBounds(150, 320, 140, 27);
         add(jTextField2);
         jTextField2.setBounds(190, 410, 100, 20);
 
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Pris:");
         add(jLabel2);
-        jLabel2.setBounds(40, 410, 100, 14);
+        jLabel2.setBounds(40, 410, 100, 17);
 
-        jTextField4.addActionListener(new java.awt.event.ActionListener() {
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField4ActionPerformed(evt);
+                jTextField1ActionPerformed(evt);
             }
         });
-        add(jTextField4);
-        jTextField4.setBounds(190, 370, 100, 20);
+        add(jTextField1);
+        jTextField1.setBounds(190, 370, 100, 20);
 
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Antal bogstaver:");
         add(jLabel4);
-        jLabel4.setBounds(40, 370, 150, 14);
+        jLabel4.setBounds(40, 370, 150, 17);
 
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
@@ -99,7 +113,7 @@ public class BestillingsOrdreGUI extends javax.swing.JPanel {
         jScrollPane1.setViewportView(jTextArea1);
 
         add(jScrollPane1);
-        jScrollPane1.setBounds(30, 80, 250, 140);
+        jScrollPane1.setBounds(30, 150, 262, 140);
 
         jCheckBox1.setBackground(new java.awt.Color(100, 100, 100));
         jCheckBox1.setForeground(new java.awt.Color(255, 255, 255));
@@ -110,7 +124,7 @@ public class BestillingsOrdreGUI extends javax.swing.JPanel {
             }
         });
         add(jCheckBox1);
-        jCheckBox1.setBounds(40, 20, 110, 23);
+        jCheckBox1.setBounds(40, 20, 80, 21);
 
         jCheckBox2.setBackground(new java.awt.Color(100, 100, 100));
         jCheckBox2.setForeground(new java.awt.Color(255, 255, 255));
@@ -121,26 +135,26 @@ public class BestillingsOrdreGUI extends javax.swing.JPanel {
             }
         });
         add(jCheckBox2);
-        jCheckBox2.setBounds(190, 20, 120, 23);
+        jCheckBox2.setBounds(130, 20, 70, 21);
 
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Skrifttype:");
         add(jLabel3);
-        jLabel3.setBounds(40, 240, 90, 14);
+        jLabel3.setBounds(330, 60, 90, 17);
 
         jLabel5.setForeground(new java.awt.Color(255, 255, 255));
         jLabel5.setText("Sten:");
         add(jLabel5);
-        jLabel5.setBounds(40, 320, 70, 14);
+        jLabel5.setBounds(40, 320, 70, 17);
 
         jComboBox3.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         add(jComboBox3);
-        jComboBox3.setBounds(450, 20, 130, 20);
+        jComboBox3.setBounds(450, 20, 140, 27);
 
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
         jLabel6.setText("Skriftstørrelse:");
         add(jLabel6);
-        jLabel6.setBounds(340, 20, 220, 14);
+        jLabel6.setBounds(330, 20, 120, 17);
 
         jTextArea3.setColumns(20);
         jTextArea3.setRows(5);
@@ -152,16 +166,60 @@ public class BestillingsOrdreGUI extends javax.swing.JPanel {
         jScrollPane3.setViewportView(jTextArea3);
 
         add(jScrollPane3);
-        jScrollPane3.setBounds(330, 80, 250, 140);
+        jScrollPane3.setBounds(330, 150, 262, 140);
 
         jLabel7.setForeground(new java.awt.Color(255, 255, 255));
         jLabel7.setText("Bemærkninger:");
         add(jLabel7);
-        jLabel7.setBounds(420, 60, 120, 14);
+        jLabel7.setBounds(390, 130, 120, 17);
 
         jButton1.setText("Opret ordre!");
         add(jButton1);
-        jButton1.setBounds(460, 410, 120, 23);
+        jButton1.setBounds(590, 410, 140, 27);
+
+        jCheckBox4.setBackground(new java.awt.Color(100, 100, 100));
+        jCheckBox4.setForeground(new java.awt.Color(255, 255, 255));
+        jCheckBox4.setText("Er dette en Bedemand?");
+        add(jCheckBox4);
+        jCheckBox4.setBounds(40, 80, 250, 21);
+        add(jTextField3);
+        jTextField3.setBounds(460, 360, 110, 27);
+
+        jLabel8.setBackground(new java.awt.Color(100, 100, 100));
+        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setText("Bestillingsdato:");
+        add(jLabel8);
+        jLabel8.setBounds(330, 320, 120, 17);
+
+        jLabel9.setBackground(new java.awt.Color(100, 100, 100));
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setText("jLabel9");
+        add(jLabel9);
+        jLabel9.setBounds(460, 320, 250, 17);
+
+        jLabel10.setBackground(new java.awt.Color(100, 100, 100));
+        jLabel10.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel10.setText("Leveringsdato:");
+        add(jLabel10);
+        jLabel10.setBounds(330, 360, 120, 17);
+
+        jLabel11.setBackground(new java.awt.Color(100, 100, 100));
+        jLabel11.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel11.setText("Rabat:");
+        add(jLabel11);
+        jLabel11.setBounds(330, 410, 100, 17);
+        add(jTextField4);
+        jTextField4.setBounds(460, 410, 110, 27);
+
+        jComboBox4.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        add(jComboBox4);
+        jComboBox4.setBounds(450, 100, 140, 27);
+
+        jLabel12.setBackground(new java.awt.Color(100, 100, 100));
+        jLabel12.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel12.setText("Skriftstil:");
+        add(jLabel12);
+        jLabel12.setBounds(330, 100, 110, 17);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
@@ -172,9 +230,9 @@ public class BestillingsOrdreGUI extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_jCheckBox2ActionPerformed
 
-    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
 
-    }//GEN-LAST:event_jTextField4ActionPerformed
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jTextArea1KeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextArea1KeyTyped
         String str = jTextArea1.getText();
@@ -193,21 +251,30 @@ public class BestillingsOrdreGUI extends javax.swing.JPanel {
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JCheckBox jCheckBox2;
     private javax.swing.JCheckBox jCheckBox3;
+    private javax.swing.JCheckBox jCheckBox4;
     private javax.swing.JComboBox jComboBox1;
     private javax.swing.JComboBox jComboBox2;
     private javax.swing.JComboBox jComboBox3;
+    private javax.swing.JComboBox jComboBox4;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextArea jTextArea3;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
     // End of variables declaration//GEN-END:variables
 }
